10555021d232d2bbf13db08a7a2af0e76698c56f
Albert Lash <albert.lash@gmail.com>
2019-08-06 21:17:34 -0400

    new

---
+++ src/templates/year.js
@@ -0,0 +1,62 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+import { css } from "@emotion/core"
+import { rhythm } from "../utils/typography"
+import Layout from "../components/layout"
+
+export default ({ data }) => (
+      <Layout>
+        <div>
+          {data.allMarkdownRemark.edges.map(({ node }) => (
+              <div key={node.id}>
+				<Link
+				  to={node.fields.slug}
+				  css={css`
+					text-decoration: none;
+					color: inherit;
+				  `
+				}
+				>
+                <h3
+                  css={css`
+                                      margin-bottom: ${rhythm(1 / 4)};
+                                                    `}
+                >
+                  {node.frontmatter.title}{" "}
+                  <span
+                    css={css`
+                                      color: #bbb;
+                                                      `}
+                  >
+                    — {node.frontmatter.date}
+                  </span>
+                </h3>
+                <p>{node.excerpt}</p>
+				</Link>
+              </div>
+            ))}
+        </div>
+      </Layout>
+)
+
+
+export const query = graphql`
+  query($dir: String!) {
+    allMarkdownRemark(filter: { fileAbsolutePath: { regex: $dir} }, sort: { fields: [frontmatter___date], order: DESC }, limit: 50) {
+      totalCount
+      edges {
+        node {
+          id
+	      fields {
+		    slug
+          }
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
